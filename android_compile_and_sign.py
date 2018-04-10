4e00d3b023d50d82f5db46ccc2690b1a4b5693a1
idanc15 <idanc15@hotmail.com>
2018-04-10 15:30:38 +0300

    Update android_compile_and_sign.py

--- android_compile_and_sign.py
+++ android_compile_and_sign.py
@@ -3,9 +3,9 @@ import sys
 
 # Execute from the Dir with the APK
 # Usage:
-# Android_compile_and_sign.py [compile/decompile] [file.apk]
+# Android_compile_and_sign.py [compile/decompile] file.apk]
 #
-# Tool in use: apktool, jar2dex, SignApk (https://github.com/techexpertize/SignApk)
+# Tool in use: apktool, jar2dex, SignApk
 # add all tools to the environment path
 
 # TODO: verify tools
@@ -17,16 +17,18 @@ action = sys.argv[1]
 apk_file = sys.argv[2]
 base_name = os.path.splitext(apk_file)[0]
 decompile = "apktool d -f " + apk_file
-app_dir = [name for name in os.listdir(".") if os.path.isdir(name) and name == base_name]
-recompile = "apktool b -f " + str(app_dir).strip('[]')
-path_to_signapk = 'C:\...\Signapk\signapk.jar'
-path_to_crt = 'C:\...\Signapk\certificate.pem'
-path_to_key = 'C:\...\Signapk]key.pk8'
-sign = "java -jar " + path_to_signapk + " " + path_to_crt + " " + path_to_key + " " + os.getcwd() + "\\" + str(app_dir).strip('[]') + "\\dist\\" + apk_file + os.getcwd() + "\\Signed.apk"
+app_dir = [name for name in os.listdir(".") if os.path.isdir(name) and name == base_name][0]
+recompile = "apktool b -f " + os.getcwd() + "\\" + app_dir
+path_to_signapk = 'C:\users\env\desktop\mobile\Signapk\signapk.jar'
+path_to_crt = 'C:\users\env\desktop\mobile\Signapk\certificate.pem'
+path_to_key = 'C:\users\env\desktop\mobile\Signapk\key.pk8'
+sign = "java -jar " + path_to_signapk + " " + path_to_crt + " " + path_to_key + " " + os.getcwd() + "\\" + app_dir + "\\dist\\" + apk_file + " " + os.getcwd() + "\\Signed.apk"
 
 
 if action == 'decompile':
     os.system(decompile)
 elif action == 'compile':
+    print "Compiling..." + recompile
     os.system(recompile)
+    print "Signing..." + sign
     os.system(sign)
